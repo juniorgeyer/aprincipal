307b825bc87af19d2aaefe5866e01e596c77b3e8
tiago-rocha09 <tiagoarrocha@gmail.com>
2019-05-31 01:23:28 -0300

    Adicionando o botão de salvar as mudanças e adicionando a função que junta todos os vetores, criando um único vetor com todos os produtos, inclusive os já modificados

--- src/app/pages/tabs1/tabs1.page.ts
+++ src/app/pages/tabs1/tabs1.page.ts
@@ -14,20 +14,20 @@ export class Tabs1Page implements OnInit {
   tour: Product[] = [];
   mom: Product[] = [];
   several: Product[] = [];
+  saveButton: boolean = false;
 
   constructor(private convertList: ConvertSuggestionsList) { }
 
   ngOnInit() {
-    this.convertList.getList().then(res=>{
-      res.subscribe(data=>{
-        this.suggestedProducts = JSON.parse(data[0].listaSugerida);
-        this.splitLists();
+      this.convertList.getList().then(res=>{
+        res.subscribe(data=>{
+          this.suggestedProducts = JSON.parse(data[0].listaSugerida);
+          this.splitLists();
+        });
       });
-    });
   }
 
   splitLists(){
-    console.log(this.suggestedProducts);
     this.suggestedProducts.map(product=>{
       if(product.category == "banho"){
         this.bath.push(product);
@@ -49,22 +49,27 @@ export class Tabs1Page implements OnInit {
       this.bath = category;
       let currentIndex: number = this.bath.indexOf(product);
       this.bath[currentIndex].quantity = +this.bath[currentIndex].quantity + 1;
+      this.showSaveButton();
     }else if(product.category == "higiene"){
       this.hygiene = category;
       let currentIndex: number = this.hygiene.indexOf(product);
       this.hygiene[currentIndex].quantity = +this.hygiene[currentIndex].quantity + 1;
+      this.showSaveButton();
     }else if(product.category == "passeio"){
       this.tour = category;
       let currentIndex: number = this.tour.indexOf(product);
       this.tour[currentIndex].quantity = +this.tour[currentIndex].quantity + 1;
+      this.showSaveButton();
     }else if(product.category == "mamae"){
       this.mom = category;
       let currentIndex: number = this.mom.indexOf(product);
       this.mom[currentIndex].quantity = +this.mom[currentIndex].quantity + 1;
+      this.showSaveButton();
     }else if(product.category == "diversos"){
       this.several = category;
       let currentIndex: number = this.several.indexOf(product);
       this.several[currentIndex].quantity = +this.several[currentIndex].quantity + 1;
+      this.showSaveButton();
     }
   }
 
@@ -74,23 +79,54 @@ export class Tabs1Page implements OnInit {
       this.bath = category;
       let currentIndex: number = this.bath.indexOf(product);
       this.bath[currentIndex].quantity = +this.bath[currentIndex].quantity - 1;
+      this.showSaveButton();
     }else if(product.category == "higiene" && product.quantity > 0){
       this.hygiene = category;
       let currentIndex: number = this.hygiene.indexOf(product);
       this.hygiene[currentIndex].quantity = +this.hygiene[currentIndex].quantity - 1;
+      this.showSaveButton();
     }else if(product.category == "passeio" && product.quantity > 0){
       this.tour = category;
       let currentIndex: number = this.tour.indexOf(product);
       this.tour[currentIndex].quantity = +this.tour[currentIndex].quantity - 1;
+      this.showSaveButton();
     }else if(product.category == "mamae" && product.quantity > 0){
       this.mom = category;
       let currentIndex: number = this.mom.indexOf(product);
       this.mom[currentIndex].quantity = +this.mom[currentIndex].quantity - 1;
+      this.showSaveButton();
     }else if(product.category == "diversos" && product.quantity > 0){
       this.several = category;
       let currentIndex: number = this.several.indexOf(product);
       this.several[currentIndex].quantity = +this.several[currentIndex].quantity - 1;
+      this.showSaveButton();
     }
+  }
+  
+  showSaveButton(){
+    this.saveButton = true;
+  }
+
+  saveChanges(){
+    let changedList: Product[] = [];
+    this.bath.map(product =>{
+      changedList.push(product);
+    });
+    this.hygiene.map(product =>{
+      changedList.push(product);
+    });
+    this.tour.map(product =>{
+      changedList.push(product);
+    });
+    this.mom.map(product =>{
+      changedList.push(product);
+    });
+    this.several.map(product =>{
+      changedList.push(product);
+    });
+
+    console.log(changedList);
+    this.saveButton = false;
   }
 }
 
