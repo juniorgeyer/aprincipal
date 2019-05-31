0d4b01c16bda9be01eb9d477adfec5068c67261d
TiagoArRocha <tiagoarrocha@gmail.com>
2019-05-30 21:36:26 -0300

    Buscando a lista de sugestões do banco, dividindo a lista em categorias e exibindo na tela por categoria. Adicionando funções de incrementar e decrementar o número

--- src/app/pages/tabs1/tabs1.page.ts
+++ src/app/pages/tabs1/tabs1.page.ts
@@ -8,20 +8,89 @@ import { ConvertSuggestionsList, Product, ProductService } from '../../services/
 })
 export class Tabs1Page implements OnInit {
 
-  suggestedProducts: Product[];
+  suggestedProducts: Product[] = [];
+  bath: Product[] = [];
+  hygiene: Product[] = [];
+  tour: Product[] = [];
+  mom: Product[] = [];
+  several: Product[] = [];
+
   constructor(private convertList: ConvertSuggestionsList) { }
 
   ngOnInit() {
     this.convertList.getList().then(res=>{
       res.subscribe(data=>{
         this.suggestedProducts = JSON.parse(data[0].listaSugerida);
-        this.verifica();
+        this.splitLists();
       });
     });
   }
 
-  verifica(){
+  splitLists(){
     console.log(this.suggestedProducts);
+    this.suggestedProducts.map(product=>{
+      if(product.category == "banho"){
+        this.bath.push(product);
+      }else if(product.category == "higiene"){
+        this.hygiene.push(product);
+      }else if(product.category == "passeio"){
+        this.tour.push(product);
+      }else if(product.category == "mamae"){
+        this.mom.push(product);
+      }else if(product.category == "diversos"){
+        this.several.push(product);
+      }
+    });
+  }
+
+  increaseQuantity(product, category){
+
+    if(product.category == "banho"){
+      this.bath = category;
+      let currentIndex: number = this.bath.indexOf(product);
+      this.bath[currentIndex].quantity = +this.bath[currentIndex].quantity + 1;
+    }else if(product.category == "higiene"){
+      this.hygiene = category;
+      let currentIndex: number = this.hygiene.indexOf(product);
+      this.hygiene[currentIndex].quantity = +this.hygiene[currentIndex].quantity + 1;
+    }else if(product.category == "passeio"){
+      this.tour = category;
+      let currentIndex: number = this.tour.indexOf(product);
+      this.tour[currentIndex].quantity = +this.tour[currentIndex].quantity + 1;
+    }else if(product.category == "mamae"){
+      this.mom = category;
+      let currentIndex: number = this.mom.indexOf(product);
+      this.mom[currentIndex].quantity = +this.mom[currentIndex].quantity + 1;
+    }else if(product.category == "diversos"){
+      this.several = category;
+      let currentIndex: number = this.several.indexOf(product);
+      this.several[currentIndex].quantity = +this.several[currentIndex].quantity + 1;
+    }
+  }
+
+  decreaseQuantity(product, category){
+
+    if(product.category == "banho" && product.quantity > 0){
+      this.bath = category;
+      let currentIndex: number = this.bath.indexOf(product);
+      this.bath[currentIndex].quantity = +this.bath[currentIndex].quantity - 1;
+    }else if(product.category == "higiene" && product.quantity > 0){
+      this.hygiene = category;
+      let currentIndex: number = this.hygiene.indexOf(product);
+      this.hygiene[currentIndex].quantity = +this.hygiene[currentIndex].quantity - 1;
+    }else if(product.category == "passeio" && product.quantity > 0){
+      this.tour = category;
+      let currentIndex: number = this.tour.indexOf(product);
+      this.tour[currentIndex].quantity = +this.tour[currentIndex].quantity - 1;
+    }else if(product.category == "mamae" && product.quantity > 0){
+      this.mom = category;
+      let currentIndex: number = this.mom.indexOf(product);
+      this.mom[currentIndex].quantity = +this.mom[currentIndex].quantity - 1;
+    }else if(product.category == "diversos" && product.quantity > 0){
+      this.several = category;
+      let currentIndex: number = this.several.indexOf(product);
+      this.several[currentIndex].quantity = +this.several[currentIndex].quantity - 1;
+    }
   }
 }
 
